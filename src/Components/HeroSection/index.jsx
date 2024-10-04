19fe8e393927ee450bceed1168c5133bfb539b68
Martinsen, Weronika <Weronika.Martinsen@akersolutions.com>
2024-10-04 12:21:11 +0200

    Styling fix

--- src/Components/HeroSection/index.jsx
+++ src/Components/HeroSection/index.jsx
@@ -6,8 +6,8 @@ function HeroSection({ onSearch }) {
       <div className="custom-max-width container">
         <div className="flex justify-start items-start flex-col pl-8 pr-8">
           <div>
-            <h1 className="text-7xl mt-8">Explore, shop</h1>
-            <h2 className="text-5xl mb-8 mt-4">repeat again.</h2>
+            <h1 className="text-5xl xl:text-6xl mt-8">Explore, shop</h1>
+            <h2 className="text-4xl xl:text-5xl mb-8 mt-4">repeat again.</h2>
             <div className="mt-2">
               <SearchBar onSearch={onSearch} />
             </div>
